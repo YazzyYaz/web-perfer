938eb6a0c0880af030da1913451e3aedec909dbf
Yaz Khoury <yaz.khoury@gmail.com>
2015-09-22 15:01:32 -0400

    chore: Move views repo to root

--- views/js/main.js
+++ views/js/main.js
@@ -449,11 +449,12 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  var pizzacontainer = document.querySelectorAll(".randomPizzaContainer");
+  var newwidth = (pizzacontainer[0].offsetWidth + dx) + 'px';
   function changePizzaSizes(size) {
-    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+    var dx = determineDx(pizzacontainer[0], size);
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      pizzacontainer[i].style.width = newwidth;
     }
   }
 
@@ -496,15 +497,21 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
-
+var items;
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
+  var scrollnum = document.body.scrollTop / 1250;
+
+
+  // Here's where we get this value only once.
+  if (typeof items === 'undefined') {
+    items = document.querySelectorAll('.mover');
+  }
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollnum + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
